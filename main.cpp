858c309d797d0a4cf799f981950d45858e3f4e0d
Soumya Satish <88482100+soumya-embold@users.noreply.github.com>
2021-11-24 18:02:40 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
-// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include "CDBParser.h"
+#include "titles.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-	const std::string& cdbFile = argv[1];
+    exclusions["is"] = 1;
+    exclusions["the"] = 1;
+    exclusions["of"] = 1;
+    exclusions["and"] = 1;
+    exclusions["a"] = 1;
+    exclusions["but"] = 1;
+    exclusions["as"] = 1;
 
-	CDBParser cdb;
-	cdb.printSourceFiles(cdbFile);
+    titles.push_back("Descent of Man");
+    titles.push_back("The Ascent of Man");
+    titles.push_back("The Old Man and The Sea");
+    titles.push_back("A Portrait of the Artist As a Young Man");
+    titles.push_back("A Man is a Man but Bubblesort IS A DOG");
+    std::sort(titles.begin(), titles.end());
 
-	return 0;
+    try
+    {
+        exclusions.at("the");
+    }
+    catch(std::out_of_range e)
+    {
+        std::cout << "Not found\n";
+    }
+    std::cout << "Exclusion?: " << isExclusion("hello") << std::endl;
+    std::cout << "Exclusion?: " << isExclusion("the") << std::endl;
+
+    for(auto i = titles.begin(); i != titles.end(); i++)
+    {
+        std::cout << *i << "\n";
+    }
+
+    return 0;
 }
